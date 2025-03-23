1d93e876a70ddb82ce4efaadb0c5edbd8d2519f3
srahalh <hola@saidrahal.com>
2025-03-23 22:42:54 +0100

    feat: update listings with new web development entry and improve key handling in ListWebsites component

--- src/components/ListWebsites.tsx
+++ src/components/ListWebsites.tsx
@@ -32,7 +32,7 @@ export default function ListWebsites() {
         "grid grid-cols-1 gap-4 md:grid-cols-2 md:gap-8 lg:grid-cols-3 xl:grid-cols-4",
       )}
     >
-      {filteredListings.map((listing) => {
+      {filteredListings.map((listing, index) => {
         const ContentWrapper = listing.website ? 'a' : 'div';
         const wrapperProps = listing.website ? {
           href: listing.website,
@@ -41,8 +41,8 @@ export default function ListWebsites() {
         } : {};
 
         return (
-          <ContentWrapper
-            key={listing.title}
+            <ContentWrapper
+            key={`${listing.title}-${index}`}
             className={cn(
               "rounded bg-background p-4 shadow",
               "flex flex-col gap-4",
